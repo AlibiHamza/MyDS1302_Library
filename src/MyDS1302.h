f192af3102a0dac9ee1d170189b1af8b9206348f
AlibiHamza <hamzaalibi95@gmail.com>
2026-02-10 00:16:18 +0100

    V1.0

--- src/MyDS1302.h
+++ src/MyDS1302.h
@@ -6,10 +6,9 @@
 
 class MyDS1302 {
     public:        
-        // Public method
-
         // Constructor with pins
         MyDS1302(uint8_t cePin, uint8_t ioPin, uint8_t sclkPin);
+        // Public method
         void begin();
         void setDateTime(uint8_t second, uint8_t minute, uint8_t hour,
                      uint8_t day, uint8_t month, uint8_t year);
@@ -28,7 +27,7 @@ class MyDS1302 {
         void _writeRegister(uint8_t reg, uint8_t data);
         uint8_t _readRegister(uint8_t reg);
 
-        // Pin
+        // Pins
         uint8_t _cePin;
         uint8_t _ioPin;
         uint8_t _sclkPin; 
